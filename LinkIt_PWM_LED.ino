a17c8c49cfaa428e58e35182e5db2a4f38131263
Abdullah Muhammad Sadiq <abdullahsadiq7@yahoo.com>
2021-08-11 12:13:10 +0400

    Add files via upload

---
+++ LinkIt_PWM_LED.ino
@@ -0,0 +1,29 @@
+#include <LBT.h>
+#include <LBTServer.h>
+
+int ledPin = 9; //Pin the LED strip is connected to the LinkIt ONE
+int value;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);   //Initialize serial port
+Serial.println("Started LED PWM control program");
+pinMode(ledPin,OUTPUT);   //Set ledPin as output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while (Serial.available() == 0){}   //Wait until user inputs something
+value = Serial.parseInt();
+Serial.println(value);
+LBTServer.write(value);
+
+if (value >=0 && value <=1023){
+  analogWrite(ledPin,value);  //Writes the value saved to the ledPin
+  }
+else {
+  Serial.println("Error! Type a value between 0-1023");
+  LBTServer.write("Error! Type a value between 0-1023");
+  }
+
+}
